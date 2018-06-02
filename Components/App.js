d88ab80ef14dc23aff2ad7f5d59314dc6eb05b22
pavanbharadwajh <pavanbharadwajhms@gmail.com>
2018-06-02 04:39:08 -0500

    Initail Commit

---
+++ Components/App.js
@@ -0,0 +1,10 @@
+import {StackNavigator} from 'react-navigation';
+import SearchPage from './SearchPage'
+import SearchResults from './SearchResults';
+
+const App = StackNavigator({
+  Home: { screen: SearchPage },
+  Results: { screen: SearchResults },
+});
+
+export default App;
---
+++ Components/SearchPage.js
@@ -0,0 +1,146 @@
+'use-strict'
+import React , {Component} from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Button,
+    Image,
+    ActivityIndicator,
+  } from 'react-native';
+
+  function urlForQueryAndPage(key, value, pageNumber) {
+    const data = {
+        country: 'uk',
+        pretty: '1',
+        encoding: 'json',
+        listing_type: 'buy',
+        action: 'search_listings',
+        page: pageNumber,
+    };
+    data[key] = value;
+  
+    const querystring = Object.keys(data)
+      .map(key => key + '=' + encodeURIComponent(data[key]))
+      .join('&');
+  
+    return  'https://api.nestoria.co.uk/api?' + querystring;
+  }
+
+  export default class SearchPage extends Component{
+      constructor(props){
+        super(props);
+        this.state = {
+          message :'',
+          searchString : 'london',
+          isLoading : false
+        };
+      }
+
+      onSearchStringChanged = (event) => {
+        this.setState({searchString: event.nativeEvent.text})
+      }
+
+      _executeQuery = (query) =>{
+        console.log(query);
+        this.setState({isLoading :true})
+        fetch(query)
+        .then(response => response.json())
+        .then(json => this._handleResponse(json.response))
+        .catch(error =>
+          this.setState({
+            isLoading: false,
+            message: 'Something bad happened ' + error
+        }));
+      }
+
+      _onSearchPressed = () =>{
+        const query = urlForQueryAndPage('place_name', this.state.searchString, 1)
+        this._executeQuery(query)
+      }
+
+      _handleResponse = (response) => {
+        this.setState({ isLoading: false , message: '' });
+        if (response.application_response_code.substr(0, 1) === '1') {
+          this.props.navigation.navigate(
+            'Results', {listings: response.listings});
+        } else {
+          this.setState({ message: 'Location not recognized; please try again.'});
+        }
+      };
+
+      static navigationOptions = {
+        title : 'Property Finder'
+      };
+
+      render() {
+        const spinner = this.state.isLoading ? <ActivityIndicator size='large'/> : null;
+        return (
+          <View style={styles.container}>
+            <Text style={styles.description}>
+              Search for houses to buy!
+            </Text>
+            <Text style={styles.description}>
+              Search by place-name or postcode.
+            </Text>
+            <View style = {styles.flowright}>
+            <TextInput
+                underlineColorAndroid={'transparent'}
+                style={styles.searchInput}
+                value = {this.state.searchString}
+                onChange = {this.onSearchStringChanged}
+                placeholder='Search via name or postcode'/>
+            <Button
+                onPress = {this._onSearchPressed}
+                color='#48BBEC'
+                title='Go'
+                />    
+            </View>
+            <Image 
+                style = {styles.image}
+                source = {require('../Resource/images/house.png')}></Image>
+            {spinner}
+            <Text style ={styles.description}>{this.state.message}</Text>
+          </View>
+        );
+      }
+  }
+
+  const styles = StyleSheet.create({
+    description: {
+      marginBottom: 20,
+      fontSize: 18,
+      textAlign: 'center',
+      color: '#656565'
+    },
+    container: {
+      padding: 30,
+      marginTop: 65,
+      alignItems: 'center'
+    },
+
+    flowright: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+    },
+
+    searchInput: {
+        height: 36,
+        padding: 4,
+        marginRight: 5,
+        flexGrow: 1,
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: '#48BBEC',
+        borderRadius: 8,
+        color: '#48BBEC',
+      },
+
+      image: {
+        width: 217,
+        height: 138,
+      },
+  });
